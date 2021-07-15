8a0bc2ffd66dd1f7b27149e50dfcae8433860c44
jioun.lee.dev <jioun.lee@nts-corp.com>
2021-07-15 22:29:30 +0900

    #1 Introduction to useState

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import React, { useState } from "react";
+
 function App() {
-  return <div className="App">HELLO REACT</div>;
+  const [item, setItem] = useState(1);
+  const incrementItem = () => setItem(item + 1);
+  const decrementItem = () => setItem(item - 1);
+
+  return (
+    <div className="App">
+      <h1>Hello {item}</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <button type="button" onClick={incrementItem}>
+        Increment
+      </button>
+      <button type="button" onClick={decrementItem}>
+        Decrement
+      </button>
+    </div>
+  );
 }
 
 export default App;
