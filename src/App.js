1b5bfe1a50a7d6e1b31a5bbb298131cd104a6536
jioun.lee.dev <jioun.lee@nts-corp.com>
2021-07-17 10:34:58 +0900

    #1 1. useInput

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React, { useState } from "react";
 
+const useInput = (initialValue) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = (event) => {
+    setValue(event.target);
+  };
+  return { value, onChange };
+};
+
 function App() {
   const [item, setItem] = useState(1);
   const incrementItem = () => setItem(item + 1);
   const decrementItem = () => setItem(item - 1);
 
+  const name = useInput("Ms.");
+
   return (
     <div className="App">
       <h1>Hello {item}</h1>
+      <input placeholder="Name" {...name} />
       <h2>Start editing to see some magic happen!</h2>
       <button type="button" onClick={incrementItem}>
         Increment
