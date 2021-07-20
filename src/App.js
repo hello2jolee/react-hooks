fd3142158e0570ee7b8f7f3af69d8a1db6f65c73
jioun.lee.dev <jioun.lee@nts-corp.com>
2021-07-20 16:25:45 +0900

    #3 1. useTitle

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useInput } from "./hooks/useInput";
 import { useTabs } from "./hooks/useTabs";
+import { useTitle } from "./hooks/useTitle";
 
 const content = [
   { tab: "Section 1", content: "I'm the content of the Section 1" },
@@ -8,6 +9,7 @@ const content = [
 ];
 
 function App() {
+  // useInput
   const [item, setItem] = useState(1);
   const incrementItem = () => setItem(item + 1);
   const decrementItem = () => setItem(item - 1);
@@ -15,16 +17,29 @@ function App() {
   const maxLen = (value) => value.length < 10;
   const name = useInput("Ms.", maxLen);
 
+  // useTabs
   const { currentItem, changeItem } = useTabs(0, content);
+
+  // useTitle
+  const titleUpdater = useTitle("Loading...");
+  setTimeout(() => titleUpdater("Home"), 5000);
 
   return (
     <div className="App">
+      {/* [S] useTitle */}
+      <div>HI</div>
+      {/* [E] useTitle */}
+
+      {/* [S] useTabs */}
       {content.map((section, index) => (
         <button type="button" key={index} onClick={() => changeItem(index)}>
           {section.tab}
         </button>
       ))}
       <div>{currentItem.content}</div>
+      {/* [E] useTabs */}
+
+      {/* [S] useInput */}
       <h1>Hello {item}</h1>
       <input placeholder="Name" {...name} />
       <h2>Start editing to see some magic happen!</h2>
@@ -34,6 +49,7 @@ function App() {
       <button type="button" onClick={decrementItem}>
         Decrement
       </button>
+      {/* [E] useInput */}
     </div>
   );
 }
